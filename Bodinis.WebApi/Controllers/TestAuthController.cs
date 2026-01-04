9c8733bcb0dab2209d3ad831fae4d17601cc4627
Agustin Campos <alaguscamp@hotmail.com>
2026-01-04 17:09:53 -0300

    authorization con JWT roles (Admin / Empleado)

---
+++ Bodinis.WebApi/Controllers/TestAuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bodinis.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestAuthController: ControllerBase
+    {
+        [HttpGet("libre")]
+        public IActionResult Libre()
+        {
+            return Ok("Endpoint libre");
+        }
+
+        [Authorize]
+        [HttpGet("protegido")]
+        public IActionResult Protegido()
+        {
+            return Ok("Estas autenticado");
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpGet("solo-admin")]
+        public IActionResult SoloAdmin()
+        {
+            return Ok("Eres admin");
+        }
+    }
+}
